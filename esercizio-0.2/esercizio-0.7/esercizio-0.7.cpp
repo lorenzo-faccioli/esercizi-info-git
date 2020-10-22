7a4e7894b812fb1de40b3ce1b3b11ef4afd956d9
unknown <lorenzofaccio01@gmail.com>
2020-10-22 15:16:05 +0200

    fatto l'esercizio esercizio-0.7

---
+++ esercizio-0.2/esercizio-0.7/esercizio-0.7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"dimmi un numrero ";
+    cin>>n;
+
+    if(n%2==0)
+        cout<<"il numero che mi hai dato e pari";
+        else
+            cout<<"il numero che mi hai dato e dispari";
+return 0;        
+}
+
+        
+    
+
